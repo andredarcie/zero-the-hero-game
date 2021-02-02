c05608517d944977cffc90e1cfb3f4629ccb097e
andredarcie <andrendarcie@gmail.com>
2021-02-01 22:14:06 -0300

    Add new levels

--- LevelBase.gd
+++ LevelBase.gd
@@ -3,7 +3,7 @@ extends Node2D
 var player_scene = preload("res://player/player.tscn")
 var hud_scene = preload("res://ui/hud.tscn")
 
-func _ready():
+func _enter_tree():
 	add_child(hud_scene.instance())
 	var player = player_scene.instance()
 	player.global_position = LevelManager.current_player_position
