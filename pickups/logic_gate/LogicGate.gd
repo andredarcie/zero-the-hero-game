f0eb294d104224b8b371e64a86fc1f02e677a078
andredarcie <andrendarcie@gmail.com>
2021-02-27 01:28:07 -0300

    Logic gate

---
+++ pickups/logic_gate/LogicGate.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+var input1: bool = false
+var input2: bool = false
+var output: bool = false
+
+func _ready():
+	add_to_group("LogicGate")
+	
+func toggle_input(name: String) -> void:
+	if name == "Input1":
+		input1 = !input1
+	elif name == "Input2":
+		input2 = !input2
+	
+	output = input1 and input2
+		
+	if output:
+		$Sprite.frame = 1
+		for child in $Output.get_overlapping_areas():
+			var parent =  child.get_parent()
+			if parent.is_in_group("Wire") and parent.has_method("on"):
+					parent.on("Up")
+					return
+	else:
+		$Sprite.frame = 0
+		for child in $Output.get_overlapping_areas():
+			var parent =  child.get_parent()
+			if parent.is_in_group("Wire") and parent.has_method("off"):
+					parent.off("Up")
+					return
+
+
+
