f289a56ccc88be1767208bd9bf1dccf75c519ebf
andredarcie <andrendarcie@gmail.com>
2021-03-09 22:17:37 -0300

    Update Npc.gd

--- npcs/npc/Npc.gd
+++ npcs/npc/Npc.gd
@@ -1,8 +1,8 @@
 class_name Npc extends Node2D
 
 var dialogBox: DialogBox = null
-var characterName: String = ''
-var dialogs = []
+var characterName: String = 'Morte'
+var dialogs = ["Cavaleiro niilista! Aceita uma partida de xadrez por sua alma?"]
 
 func _ready():
 	dialogBox = get_tree().get_root().find_node("DialogBox", true, false) as DialogBox
