e5811dcc69d7f21be41ebeca6173e1150c6f024d
andredarcie <andrendarcie@gmail.com>
2021-03-23 21:02:55 -0300

    Bat

--- enemies/Bat/Bat.gd
+++ enemies/Bat/Bat.gd
@@ -3,4 +3,4 @@ class_name Bat extends Enemy
 func _ready():
 	sprite_default = preload("res://enemies/Bat/bat.png")
 	sprite_hurt = preload("res://enemies/Bat/bat_hurt.png")
-	health =  10
+	health =  1
