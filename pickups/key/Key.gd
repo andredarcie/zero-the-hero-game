c1632523fdb655573a45caf94b5a39af28fb920a
andredarcie <andrendarcie@gmail.com>
2021-03-23 21:01:13 -0300

    New key

--- pickups/key/Key.gd
+++ pickups/key/Key.gd
@@ -2,5 +2,9 @@ class_name Key extends Pickup
 
 func _on_Key_body_entered(body):
 	if GameState.check_body_is_player(body) && GameState.keys < 9:
-		GameState.keys += 1
-		queue_free()
+		$AnimationPlayer.play("get")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	GameState.keys += 1
+	queue_free()
