7d1d44cd5b6688ed8de297952ddf68e57dbe1bfb
andredarcie <andrendarcie@gmail.com>
2021-03-23 21:02:47 -0300

    Add new vase

--- pickups/vase/Vase.gd
+++ pickups/vase/Vase.gd
@@ -2,17 +2,21 @@ extends Area2D
 
 var rng = RandomNumberGenerator.new()
 onready var coin = preload("res://pickups/coin/Coin.tscn")
+var broken: bool = false
 
 func _on_Vase_area_entered(area):
-	var parent = area.get_parent()
-	if parent.name == "sword":
-		rng.randomize()
-		var my_random_number = rng.randi_range(0, 1)
-		
-		if my_random_number == 1:
-			var new_coin = coin.instance()
-			new_coin.position = global_position
-			get_tree().root.get_child(2).add_child(new_coin)
+	if not broken:
+		var parent = area.get_parent()
+		if parent.name == "sword":
+			rng.randomize()
+			var my_random_number = rng.randi_range(0, 1)
 			
-			
-		queue_free()
+			if my_random_number == 1:
+				var new_coin = coin.instance()
+				new_coin.position = global_position
+				get_tree().root.get_child(2).add_child(new_coin)
+				
+				
+			$AnimatedSprite.play("breaking")
+			$StaticBody2D.queue_free()
+			broken = true
