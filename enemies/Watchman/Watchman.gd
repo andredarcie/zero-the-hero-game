2a922428c4f2199d22361ff87482a2e95afda499
andredarcie <andrendarcie@gmail.com>
2021-04-04 19:13:45 -0300

    New field of view system

--- enemies/Watchman/Watchman.gd
+++ enemies/Watchman/Watchman.gd
@@ -1,42 +1,40 @@
 class_name Watchman extends Enemy
 
-var saw_the_player: bool = false
-var debug_mode: bool = true
+var player = null
+const FOV_TOLERANCE = 25
+const MAX_DETECTION_RANGE = 60
 
 func _ready():
+	player = get_node("/root").find_node("Player", true, false)
 	move_random_direction = false
 	$Sprite2.frame = 0
 	
-	if not debug_mode:
-		turn_off_debug_mode()
-	
 func _physics_process(delta):	
-	var saw_the_player_1 = check_raycast_collission($Vision)
-	var saw_the_player_2 = check_raycast_collission($Vision2)
-	var saw_the_player_3 = check_raycast_collission($Vision3)
-	var saw_the_player_4 = check_raycast_collission($Vision4)	
-	var saw_the_player_5 = check_raycast_collission($Vision5)
-	var saw_the_player_6 = check_raycast_collission($Vision5)	
-	var saw_the_player_7 = check_raycast_collission($Vision7)
-		
-	if saw_the_player_1 or saw_the_player_2 or saw_the_player_3 or saw_the_player_4 or saw_the_player_5 or saw_the_player_6 or saw_the_player_7:
+	if player_in_field_of_view() and player_line_of_sight():
 		$Sprite2.frame = 1
 	else:
 		$Sprite2.frame = 0
 
-
-func check_raycast_collission(ray_cast : RayCast2D):
-	if ray_cast.get_collider():
-		if GameState.check_body_is_player(ray_cast.get_collider()):
-			return true
-		else:
-			return false
+func player_in_field_of_view() -> bool:
+	var npc_facing_direction = Vector2.DOWN.rotated(global_rotation)
+	var direction_to_player = (player.global_position - global_position).normalized()
+	
+	if abs(direction_to_player.angle_to(npc_facing_direction)) < deg2rad(FOV_TOLERANCE):
+		return true
 	else:
 		return false
 
-
-func turn_off_debug_mode():
-	for child in self.get_children():
-		if 'Vision' in child.name:
-			for sub_child in child.get_children():
-				sub_child.queue_free()
+func player_line_of_sight():
+	var space = get_world_2d().direct_space_state
+	var LOS_obstacle = space.intersect_ray(global_position, player.global_position, [self], collision_mask)
+	
+	if not LOS_obstacle:
+		return false
+	
+	var distance_to_player = player.global_position.distance_to(global_position)
+	var Player_in_Range = distance_to_player < MAX_DETECTION_RANGE
+	
+	if LOS_obstacle.collider == player and Player_in_Range:
+		return true
+	else:
+		return false
