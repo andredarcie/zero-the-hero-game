41279d622dc0d8b8bcb71152be4ae7c042849a6d
andredarcie <andrendarcie@gmail.com>
2021-04-09 22:41:12 -0300

    Update Watchman.gd

--- enemies/Watchman/Watchman.gd
+++ enemies/Watchman/Watchman.gd
@@ -9,7 +9,7 @@ func _ready():
 	move_random_direction = false
 	$Sprite2.frame = 0
 	
-func _physics_process(delta):	
+func _physics_process(_delta):	
 	if player_in_field_of_view() and player_line_of_sight():
 		$Sprite2.frame = 1
 	else:
@@ -19,12 +19,9 @@ func player_in_field_of_view() -> bool:
 	var npc_facing_direction = Vector2.DOWN.rotated(global_rotation)
 	var direction_to_player = (player.global_position - global_position).normalized()
 	
-	if abs(direction_to_player.angle_to(npc_facing_direction)) < deg2rad(FOV_TOLERANCE):
-		return true
-	else:
-		return false
+	return abs(direction_to_player.angle_to(npc_facing_direction)) < deg2rad(FOV_TOLERANCE)
 
-func player_line_of_sight():
+func player_line_of_sight() -> bool:
 	var space = get_world_2d().direct_space_state
 	var LOS_obstacle = space.intersect_ray(global_position, player.global_position, [self], collision_mask)
 	
@@ -34,7 +31,4 @@ func player_line_of_sight():
 	var distance_to_player = player.global_position.distance_to(global_position)
 	var Player_in_Range = distance_to_player < MAX_DETECTION_RANGE
 	
-	if LOS_obstacle.collider == player and Player_in_Range:
-		return true
-	else:
-		return false
+	return LOS_obstacle.collider == player and Player_in_Range
