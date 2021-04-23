d81ab9841507529eb62c385301f71bbae526f24c
andredarcie <andrendarcie@gmail.com>
2021-04-22 22:50:31 -0300

    Update Undead.gd

--- enemies/Undead/Undead.gd
+++ enemies/Undead/Undead.gd
@@ -4,7 +4,10 @@ var follow_player: bool = false
 var move: Vector2 = Vector2.ZERO
 var player
 onready var Ballon = $Balloon
+onready var InterrogationTime = $InterrogationTime
 var alert_texture: Texture = preload("res://enemies/Enemy/Balloons/alert.png")
+var suspicious_texture: Texture = preload("res://enemies/Enemy/Balloons/suspicious.png")
+var player_is_on_vision: bool = false
 
 func _ready():
 	speed = 20
@@ -15,12 +18,18 @@ func _ready():
 func _physics_process(delta):
 	move = Vector2.ZERO
 	
-	if follow_player:
-		if is_hurt:
-			move_and_slide(pushed_move_direction.normalized() * (speed * 2))
-		else:
-			move = global_position.direction_to(player.global_position) * (speed * 2)
-			move_and_slide(move, Vector2(0, 0))
+	if is_hurt:
+		move_and_slide(pushed_move_direction.normalized() * (speed * 2))
+	elif player_is_on_vision and GameState.check_line_of_sight(self, player):
+		enemy_saw_player()
+		move = global_position.direction_to(player.global_position) * (speed * 2)
+		move_and_slide(move, Vector2(0, 0))
+	else:
+		if not move_random_direction:
+			Ballon.texture = suspicious_texture
+			InterrogationTime.start()
+			
+		enemy_lost_sight_of_player()
 
 
 func _on_Vision_body_entered(body):
@@ -28,14 +37,22 @@ func _on_Vision_body_entered(body):
 		player = GameState.get_player()
 		
 	if GameState.check_body_is_player(body):
-		if GameState.check_line_of_sight(self, player):
-			Ballon.texture = alert_texture
-			follow_player = true
-			move_random_direction = false
+		player_is_on_vision = true
 
 
 func _on_Vision_body_exited(body):
 	if GameState.check_body_is_player(body):
-		Ballon.texture = null
-		follow_player = false
-		move_random_direction = true
+		player_is_on_vision = false
+
+	
+func enemy_saw_player():
+	Ballon.texture = alert_texture
+	move_random_direction = false
+	
+	
+func enemy_lost_sight_of_player():
+	move_random_direction = true
+
+
+func _on_InterrogationTime_timeout():
+	Ballon.texture = null
