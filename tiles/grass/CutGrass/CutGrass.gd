f7d2e9c273b3fcdc56463fd9d91fbf63bee0da1c
andredarcie <andrendarcie@gmail.com>
2021-04-25 12:47:52 -0300

    Animated grass

--- tiles/grass/CutGrass/CutGrass.gd
+++ tiles/grass/CutGrass/CutGrass.gd
@@ -1,7 +1,8 @@
 extends Sprite
-
+	
 func _on_Area2D_area_entered(area):
 	if area.get_parent().name == "sword":
-		frame = 1
 		$StaticBody2D.queue_free()
 		$Area2D.queue_free()
+		$AnimatedSprite.queue_free()
+		$Sprite.visible = true
