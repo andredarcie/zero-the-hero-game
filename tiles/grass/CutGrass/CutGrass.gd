e4f29f4f68faf912a7e63fb18a252e37e2d48c12
andredarcie <andrendarcie@gmail.com>
2021-04-25 19:07:22 -0300

    Add new style for forests

--- tiles/grass/CutGrass/CutGrass.gd
+++ tiles/grass/CutGrass/CutGrass.gd
@@ -1,8 +1,17 @@
 extends Sprite
-	
+
+var cut: bool = false
+
 func _on_Area2D_area_entered(area):
 	if area.get_parent().name == "sword":
 		$StaticBody2D.queue_free()
 		$Area2D.queue_free()
+		$AnimatedSprite.speed_scale = 4
+		$AnimatedSprite.play("cuting")
+		cut = true
+
+
+func _on_AnimatedSprite_animation_finished():
+	if cut:
 		$AnimatedSprite.queue_free()
 		$Sprite.visible = true
