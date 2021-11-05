84f4ed570a26b99a93afe661081e9060205cbaf8
andrendarcie <andrendarcie@gmail.com>
2021-11-04 22:08:33 -0300

    Persistencia do fogo e ponte e bioma deserto

--- pickups/craft/Craft.gd
+++ pickups/craft/Craft.gd
@@ -1,19 +1,35 @@
 extends Node2D
 
-var wood = 5
-enum ThingToBuild {Bonfire, Dridge}
+var wood = 1
+enum ThingToBuild {Bonfire, Bridge}
 export(ThingToBuild) var thing_to_build
+var builded: bool = false
 
 func _ready():
 	match thing_to_build:
 		ThingToBuild.Bonfire:
+			$Water.queue_free()
 			$Bonfire/CollisionShape2D.call_deferred("set", "disabled", true)
 			$Bonfire.visible = true
 			$Bonfire/AnimatedSprite.visible = false
 			$Bonfire/Base.visible = true
 			$Bonfire/Light2D.visible = false
+			
+			if GameState.create_check(self):
+				build_bonfire()
+			
+		ThingToBuild.Bridge:
+			$Water.visible = true			
+			$Bonfire.queue_free()
+			
+			if GameState.create_check(self):
+				build_bridge()
+
 
 func _process(delta):
+	if builded:
+		return
+		
 	if GameState.player_wood >= wood:
 		$Area2D/Sprite.modulate.a = 255
 
@@ -21,30 +37,34 @@ func _on_Area2D_body_entered(body):
 	if GameState.check_body_is_player(body):
 		if GameState.player_wood >= wood:
 			GameState.player_wood = GameState.player_wood - wood
-		
-			$Area2D.queue_free()
 			
 			match thing_to_build:
 				ThingToBuild.Bonfire:
-					$Bonfire/CollisionShape2D.call_deferred("set", "disabled", false)
-					$Bonfire/AnimatedSprite.visible = true
-					$Bonfire/Base.visible = false
-					$Bonfire/Light2D.visible = true
+					build_bonfire()
 					SoundEffects.play_switch_sound()
-				ThingToBuild.Dridge:
-					$BridgeArea2D/Sprite.visible = true
-					for _i in $BridgeArea2D.get_overlapping_bodies():
-						var staticBody2D = _i.get_parent().get_node("StaticBody2D")
-						if staticBody2D:
-							staticBody2D.queue_free()
-					
+				ThingToBuild.Bridge:
+					build_bridge()
 					SoundEffects.play_bridge_sound()
-			
+					
+			GameState.destroy(self)
 		else:
 			var player = GameState.get_player()
 			player.show_ballon_wood()
 		
 
+func build_bonfire():
+	$Area2D.queue_free()
+	$Bonfire/CollisionShape2D.call_deferred("set", "disabled", false)
+	$Bonfire/AnimatedSprite.visible = true
+	$Bonfire/Base.visible = false
+	$Bonfire/Light2D.visible = true
+	builded = true
+
+func build_bridge():
+	$Area2D.queue_free()
+	$BridgeArea2D/Sprite.visible = true
+	$Water/StaticBody2D.queue_free()
+	builded = true
 
 func _on_Area2D_body_exited(body):
 	if GameState.check_body_is_player(body):
