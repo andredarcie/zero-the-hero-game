975c8edb6c40b6fff3bea230141bab67222c72f8
andrendarcie <andrendarcie@gmail.com>
2022-06-28 20:26:30 -0300

    Adiciona novo level base

--- levels/base/LevelBaseForest.gd
+++ levels/base/LevelBaseForest.gd
@@ -1,10 +1,27 @@
-extends Node
+extends Node2D
 
 var current_level = 8
 var current_player_position = Vector2(40,40)
+var player_scene = preload("res://player/Player/Player.tscn")
 
 func set_current_level_position():
 	return
+
+func _enter_tree():
+	Hud.hud_visible(true)
+		
+	LevelManager.set_current_level_position()
+	
+	var player = player_scene.instance()
+	player.global_position = LevelManager.current_player_position
+	player.change_item(GameState.player_slot_item, false)
+	
+	if name.find("d") >= 0:
+		player.turn_dark()
+	else:
+		player.turn_light()
+		
+	add_child(player)
 	
 func go_to_next_level():
 	current_player_position = Vector2(154,210)
